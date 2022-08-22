251d86998996d29fd2d4e6800ed1cab4e0a1b084
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-08-22 18:51:49 +0900

    Feat : initial settings

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Footer, Header, NavBar } from './components/common';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <NavBar />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function App() {
-  return <div className="App">돈모아</div>;
-}
-
-export default App;
