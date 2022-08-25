0b7afc2b13f16ec61583d1d6bff9df4c70dd4d4e
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-08-25 09:01:43 +0900

    Feat : colors 객체 생성 및 적용, react icons 적용

--- client/src/components/calculators/GeneralCal/GeneralCal.tsx
+++ client/src/components/calculators/GeneralCal/GeneralCal.tsx
@@ -1,31 +1,46 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { MdOutlineTimer } from 'react-icons/md';
 
 export const GeneralCal = () => {
+  const colors = {
+    blue: '#adbae8',
+    red: '#cd3e38',
+    beige: '#daaaa1',
+    black: '#010101',
+    lightBlack: '#171717',
+  };
+
+  const styleAlignCenter =
+    'display: flex; align-items: center; justify-content: center;';
+
   const CalBody = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    ${styleAlignCenter}
     flex-direction: column;
     margin: 0 auto;
     width: 250px;
   `;
 
   const CalControlButtonArea = styled.div`
+    ${styleAlignCenter}
+    justify-content: space-around;
     width: 100%;
-    background-color: #010101;
-    color: #adbae8;
+    background-color: ${colors.black};
+    color: ${colors.blue};
+  `;
+
+  const CalControlButton = styled.button`
+    border: none;
+    background: none;
+    color: white;
   `;
 
   const CalButtonArea = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    ${styleAlignCenter}
     flex-direction: column;
     padding: 15px 0;
     width: 100%;
-    background-color: #010101;
+    background-color: ${colors.black};
   `;
 
   const CalButtonRow = styled.div``;
@@ -36,45 +51,50 @@ export const GeneralCal = () => {
     margin: 2px;
     border: none;
     border-radius: 100%;
-    background-color: #171717;
-    color: #adbae8;
+    background-color: ${colors.lightBlack};
+    color: ${colors.blue};
     font-size: 20px;
   `;
 
   return (
     <CalBody>
       <div>input 영역</div>
-      <CalControlButtonArea>control btns 영역</CalControlButtonArea>
+      <CalControlButtonArea>
+        <CalControlButton>
+          <MdOutlineTimer />
+        </CalControlButton>
+        <CalControlButton>지우기 아이콘</CalControlButton>
+      </CalControlButtonArea>
       <CalButtonArea>
         <div>
-          <CalButton style={{ color: '#cd3e38' }}>C</CalButton>
-          <CalButton style={{ color: '#daaaa1' }}>()</CalButton>
-          <CalButton style={{ color: '#daaaa1' }}>%</CalButton>
-          <CalButton style={{ color: '#daaaa1' }}>÷</CalButton>
+          <CalButton style={{ color: `${colors.red}` }}>C</CalButton>
+          <CalButton style={{ color: `${colors.beige}` }}>()</CalButton>
+          <CalButton style={{ color: `${colors.beige}` }}>%</CalButton>
+          <CalButton style={{ color: `${colors.beige}` }}>÷</CalButton>
         </div>
         <div>
           <CalButton>7</CalButton>
           <CalButton>8</CalButton>
           <CalButton>9</CalButton>
-          <CalButton style={{ color: '#daaaa1' }}>×</CalButton>
+          <CalButton style={{ color: `${colors.beige}` }}>×</CalButton>
         </div>
         <div>
           <CalButton>4</CalButton>
           <CalButton>5</CalButton>
           <CalButton>6</CalButton>
-          <CalButton style={{ color: '#daaaa1' }}>－</CalButton>
+          <CalButton style={{ color: `${colors.beige}` }}>－</CalButton>
         </div>
         <div>
           <CalButton>1</CalButton>
           <CalButton>2</CalButton>
           <CalButton>3</CalButton>
-          <CalButton style={{ color: '#daaaa1' }}>＋</CalButton>
+          <CalButton style={{ color: `${colors.beige}` }}>＋</CalButton>
         </div>
         <div>
           <CalButton>+/-</CalButton>
           <CalButton>0</CalButton>
           <CalButton>.</CalButton>
-          <CalButton style={{ color: '#daaaa1' }}>＝</CalButton>
+          <CalButton style={{ color: `${colors.beige}` }}>＝</CalButton>
         </div>
       </CalButtonArea>
     </CalBody>
